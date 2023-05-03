c02067367bbf86a6c2930ecee5abbc84bb416d7d
Sun Hao <szzxsh@163.com>
2023-05-03 15:28:42 +0800

    update main

--- main.py
+++ main.py
@@ -303,13 +303,13 @@ def process_race(race_mode=0):
                         pro.press_buttons(Buttons.Y)
                     elif elapsed > 22 and elapsed < 24:
                         pro.press_buttons(Buttons.DPAD_LEFT)
-                    elif elapsed > 46 and elapsed < 48:
+                    elif elapsed >= 46 and elapsed < 48:
                         pro.press_buttons(Buttons.B, 5)
                         pro.press_buttons(Buttons.Y)
                         pro.press_buttons(Buttons.Y)
                     elif elapsed > 60:
                         break
-                    elif elapsed > 24 and elapsed < 60 or elapsed < 10:
+                    elif elapsed > 24 and elapsed < 46 or elapsed < 10:
                         pro.press_button(Buttons.Y, 0.7)
                         pro.press_button(Buttons.Y, 0)
                         time.sleep(3)
