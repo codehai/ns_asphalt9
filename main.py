5bef9a9efa2d85558aa22bdfaec4b5803ca58361
Sun Hao <szzxsh@163.com>
2023-05-03 14:14:25 +0800

    update main

--- main.py
+++ main.py
@@ -295,7 +295,7 @@ def process_race(race_mode=0):
                     elapsed = end - start + delta
                     logger.info(f"elapsed = {elapsed}")
                     if elapsed >= 14.5 and elapsed <= 15.5:
-                        pro.press_buttons(Buttons.B, 6, block=False)
+                        pro.press_buttons(Buttons.B, 5)
                     elif elapsed >= 17 and elapsed <= 19:
                         pro.press_buttons(Buttons.DPAD_LEFT)
                     elif elapsed >=21 and elapsed <= 22:
