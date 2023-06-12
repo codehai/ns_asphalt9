5d8c5ee737ca648b425346f8039b8409fc9ef50c
sh <wmpksb@gmail.com>
2023-06-12 16:03:02 +0800

    feat: 截图支持MJPG格式

--- screenshot.py
+++ screenshot.py
@@ -2,11 +2,28 @@ from subprocess import Popen, PIPE
 from utils.log import logger
 
 
+def get_format():
+    cmd = "v4l2-ctl --list-formats"
+    p = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True)
+    output, error = p.communicate()
+    if output:
+        result = output.decode("utf-8")
+        if "MJPG" in result:
+            return "MJPG", "jpeg"
+        elif "YUYV" in result:
+            return "YUYV", "yuv"
+        else:
+            raise Exception("Not support formats type")
+    if error:
+        raise Exception(error.decode("utf-8"))
+
+
 def screenshot():
-    cmd = "v4l2-ctl --device /dev/video0 --set-fmt-video=width=1920,height=1080,pixelformat=YUYV --stream-mmap --stream-to=./output.yuv --stream-count=1"
+    format, file_type = get_format()
+    cmd = f"v4l2-ctl --device /dev/video0 --set-fmt-video=width=1920,height=1080,pixelformat={format} --stream-mmap --stream-to=./source.{file_type} --stream-count=1"
     p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE, cwd="./images")
     p.wait()
-    cmd = "ffmpeg -loglevel quiet -y -s 1920*1080 -pix_fmt yuyv422 -i ./output.yuv ./output.jpg"
+    cmd = f"ffmpeg -loglevel quiet -y -s 1920*1080 -i ./source.{file_type} ./output.jpg"
     p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE, cwd="./images")
     p.wait()
 
