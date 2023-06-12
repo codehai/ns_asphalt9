6e9da6327b9f70206cd9ddba973c10d268aebf57
sh <wmpksb@gmail.com>
2023-06-12 17:56:51 +0800

    imp

--- screenshot.py
+++ screenshot.py
@@ -1,14 +1,9 @@
-from v4l2 import capture
 from subprocess import Popen, PIPE
 from utils.log import logger
 
 
 def screenshot():
-    # capture.screen()
-    cmd = "v4l2-ctl --device /dev/video0 --set-fmt-video=width=1920,height=1080,pixelformat=YUYV --stream-mmap --stream-to=./output.yuv --stream-count=1"
-    p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE, cwd="./images")
-    p.wait()
-    cmd = "ffmpeg -loglevel quiet -y -s 1920*1080 -pix_fmt yuyv422 -i ./output.yuv ./output.jpg"
+    cmd = "ffmpeg -loglevel quiet -y -f v4l2 -i /dev/video0 -frames:v 1 output.jpg"
     p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE, cwd="./images")
     p.wait()
 
