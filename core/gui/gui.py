180046ef495d41aaa846aafd00364b15d0e1ab61
sh <wmpksb@gmail.com>
2023-06-26 17:41:24 +0800

    imp

--- core/gui/gui.py
+++ core/gui/gui.py
@@ -65,7 +65,7 @@ class App(customtkinter.CTk):
         )
         self.home_button.grid(row=1, column=0, sticky="ew")
 
-        self.frame_2_button = customtkinter.CTkButton(
+        self.settings_button = customtkinter.CTkButton(
             self.navigation_frame,
             corner_radius=0,
             height=40,
@@ -76,11 +76,11 @@ class App(customtkinter.CTk):
             hover_color=("gray70", "gray30"),
             image=self.chat_image,
             anchor="w",
-            command=self.frame_2_button_event,
+            command=self.settings_button_event,
         )
-        self.frame_2_button.grid(row=2, column=0, sticky="ew")
+        self.settings_button.grid(row=2, column=0, sticky="ew")
 
-        self.frame_3_button = customtkinter.CTkButton(
+        self.help_button = customtkinter.CTkButton(
             self.navigation_frame,
             corner_radius=0,
             height=40,
@@ -91,9 +91,9 @@ class App(customtkinter.CTk):
             hover_color=("gray70", "gray30"),
             image=self.add_user_image,
             anchor="w",
-            command=self.frame_3_button_event,
+            command=self.help_button_event,
         )
-        self.frame_3_button.grid(row=3, column=0, sticky="ew")
+        self.help_button.grid(row=3, column=0, sticky="ew")
 
         # create home frame
         self.home_frame = customtkinter.CTkFrame(
@@ -112,75 +112,43 @@ class App(customtkinter.CTk):
         )
 
         # create second frame
-        self.second_frame = customtkinter.CTkScrollableFrame(
+        self.settings = customtkinter.CTkScrollableFrame(
             self, corner_radius=0, fg_color="transparent"
         )
 
-        # # create scrollable frame
-        # self.scrollable_frame = customtkinter.CTkScrollableFrame(
-        #     self, label_text="CTkScrollableFrame"
-        # )
-        # self.scrollable_frame.grid(
-        #     row=1, column=2, padx=(20, 0), pady=(20, 0), sticky="nsew"
-        # )
-        # self.scrollable_frame.grid_columnconfigure(0, weight=1)
-        # self.scrollable_frame_switches = []
+        for row, label_text in enumerate(["模式", "任务", "多一", "多二", "寻车"]):
+            label = customtkinter.CTkLabel(master=self.settings, text=f"{label_text}:")
+            label.grid(row=row, column=0, columnspan=1, padx=10, pady=10, sticky="")
 
-        self.label_mode = customtkinter.CTkLabel(master=self.second_frame, text="模式:")
-        self.label_mode.grid(row=0, column=0, columnspan=1, padx=10, pady=10, sticky="")
-
-        self.label_mode = customtkinter.CTkLabel(master=self.second_frame, text="任务:")
-        self.label_mode.grid(row=1, column=0, columnspan=1, padx=10, pady=10, sticky="")
-
-        self.label_mode = customtkinter.CTkLabel(master=self.second_frame, text="多一:")
-        self.label_mode.grid(row=4, column=0, columnspan=1, padx=10, pady=10, sticky="")
-
-        # self.label_mode = customtkinter.CTkLabel(master=self.second_frame, text="多二:")
-        # self.label_mode.grid(row=3, column=0, columnspan=1, padx=10, pady=10, sticky="")
-
-        # self.label_mode = customtkinter.CTkLabel(master=self.second_frame, text="寻车:")
-        # self.label_mode.grid(row=4, column=0, columnspan=1, padx=10, pady=10, sticky="")
-
-        self.mode_buttons = customtkinter.CTkSegmentedButton(self.second_frame)
+        self.mode_buttons = customtkinter.CTkSegmentedButton(self.settings)
         self.mode_buttons.grid(
             row=0, column=1, padx=(20, 10), pady=(10, 10), sticky="ew"
         )
 
-        self.task_1 = customtkinter.CTkCheckBox(master=self.second_frame, text="免费抽卡")
-        self.task_1.grid(row=1, column=1, pady=(10, 10), padx=(20, 0), sticky="ew")
+        tasks_frame = customtkinter.CTkFrame(self.settings, width=340)
+        tasks_frame.grid(row=1, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
 
-        self.task_combobox_1 = customtkinter.CTkComboBox(
-            self.second_frame, values=["180", "240"]
-        )
-        self.task_combobox_1.grid(row=1, column=2, padx=0, pady=(10, 10))
+        tasks = [("免费抽卡", ["180", "240"]), ("大奖赛抽卡", ["240"]), ("寻车", ["10"])]
+        for row, (task, values) in enumerate(tasks):
+            task_box = customtkinter.CTkCheckBox(master=tasks_frame, text=task)
+            task_box.grid(row=row, column=1, pady=(10, 10), padx=(20, 0), sticky="ew")
 
-        self.task_2 = customtkinter.CTkCheckBox(master=self.second_frame, text="大奖赛抽卡")
-        self.task_2.grid(row=2, column=1, pady=(10, 10), padx=(20, 0), sticky="ew")
-
-        self.task_combobox_2 = customtkinter.CTkComboBox(
-            self.second_frame, values=["240"]
-        )
-        self.task_combobox_2.grid(row=2, column=2, padx=0, pady=(10, 10))
-
-        self.task_3 = customtkinter.CTkCheckBox(master=self.second_frame, text="寻车")
-        self.task_3.grid(row=3, column=1, pady=(10, 10), padx=(20, 0), sticky="ew")
-
-        self.task_combobox_2 = customtkinter.CTkComboBox(
-            self.second_frame, values=["10"]
-        )
-        self.task_combobox_2.grid(row=3, column=2, padx=0, pady=(10, 10))
+            task_combobox = customtkinter.CTkComboBox(
+                tasks_frame, values=values, width=100
+            )
+            task_combobox.grid(row=row, column=2, padx=(20, 100), pady=(10, 10))
 
         # create tabview
-        self.tabview = customtkinter.CTkTabview(self.second_frame, width=340)
+        self.tabview = customtkinter.CTkTabview(self.settings, width=340)
         self.tabview.grid(
-            row=4, column=1, columnspan=2, padx=(20, 0), pady=(20, 0), sticky="nsew"
+            row=2, column=1, columnspan=2, padx=(20, 0), pady=(20, 0), sticky="nsew"
         )
 
         tabs = ["青铜", "白银", "黄金", "铂金"]
         for tab_index, tab_name in enumerate(tabs):
             self.tabview.add(tab_name)
 
-            self.tabview.tab(tab_name).grid_columnconfigure(1, weight=1)
+            # self.tabview.tab(tab_name).grid_columnconfigure(1, weight=1)
 
             car_level = customtkinter.CTkLabel(
                 master=self.tabview.tab(tab_name), text="车库等级:"
@@ -191,6 +159,8 @@ class App(customtkinter.CTk):
                 self.tabview.tab(tab_name),
                 dynamic_resizing=False,
                 values=tabs[: tab_index + 1],
+                width=100,
+                height=28,
             )
             option_level.grid(row=0, column=1, padx=10, pady=(10, 10))
 
@@ -202,11 +172,11 @@ class App(customtkinter.CTk):
 
             row = customtkinter.CTkLabel(master=self.tabview.tab(tab_name), text="row")
 
-            row.grid(row=1, column=1, padx=(0, 0), pady=(10, 10), sticky="nsew")
+            row.grid(row=1, column=1, padx=10, pady=(10, 10), sticky="nsew")
 
             col = customtkinter.CTkLabel(master=self.tabview.tab(tab_name), text="col")
 
-            col.grid(row=1, column=2, padx=(0, 10), pady=(10, 10), sticky="nsew")
+            col.grid(row=1, column=2, padx=10, pady=(10, 10), sticky="nsew")
 
             for r in range(6):
                 option1 = customtkinter.CTkOptionMenu(
@@ -225,36 +195,29 @@ class App(customtkinter.CTk):
                     height=28,
                 )
 
-                option1.grid(row=r + 2, column=1, padx=(0, 0), pady=(10, 10))
-                option2.grid(row=r + 2, column=2, padx=(0, 0), pady=(10, 10))
+                option1.grid(row=r + 2, column=1, padx=(10, 10), pady=(10, 10))
+                option2.grid(row=r + 2, column=2, padx=(10, 10), pady=(10, 10))
 
-        self.label_mode = customtkinter.CTkLabel(master=self.second_frame, text="多二:")
-        self.label_mode.grid(row=5, column=0, columnspan=1, padx=10, pady=10, sticky="")
-
-        tabview = customtkinter.CTkTabview(self.second_frame, width=340)
-        tabview.grid(
-            row=5, column=1, columnspan=2, padx=(20, 0), pady=(20, 0), sticky="nsew"
+        mp2_settings_frame = customtkinter.CTkFrame(self.settings, width=340)
+        mp2_settings_frame.grid(
+            row=3, column=1, columnspan=2, padx=(20, 0), pady=(20, 0), sticky="nsew"
         )
 
-        tabview.add("多二配置")
+        car_position = customtkinter.CTkLabel(master=mp2_settings_frame, text="车库位置:")
 
-        tabview.tab("多二配置").grid_columnconfigure(1, weight=1)
+        car_position.grid(row=1, column=0, padx=(15, 10), pady=(10, 10))
 
-        car_position = customtkinter.CTkLabel(master=tabview.tab("多二配置"), text="车库位置:")
-
-        car_position.grid(row=1, column=0, padx=10, pady=(10, 10))
-
-        row = customtkinter.CTkLabel(master=tabview.tab("多二配置"), text="row")
+        row = customtkinter.CTkLabel(master=mp2_settings_frame, text="row")
 
         row.grid(row=1, column=1, padx=(0, 0), pady=(10, 10), sticky="nsew")
 
-        col = customtkinter.CTkLabel(master=tabview.tab("多二配置"), text="col")
+        col = customtkinter.CTkLabel(master=mp2_settings_frame, text="col")
 
         col.grid(row=1, column=2, padx=(0, 10), pady=(10, 10), sticky="nsew")
 
         for r in range(6):
             option1 = customtkinter.CTkOptionMenu(
-                tabview.tab("多二配置"),
+                mp2_settings_frame,
                 dynamic_resizing=False,
                 values=[str(i) for i in range(0, 3)],
                 width=100,
@@ -262,43 +225,38 @@ class App(customtkinter.CTk):
             )
 
             option2 = customtkinter.CTkOptionMenu(
-                tabview.tab("多二配置"),
+                mp2_settings_frame,
                 dynamic_resizing=False,
                 values=[str(i) for i in range(0, 20)],
                 width=100,
                 height=28,
             )
 
-            option1.grid(row=r + 2, column=1, padx=(0, 0), pady=(10, 10))
-            option2.grid(row=r + 2, column=2, padx=(0, 0), pady=(10, 10))
+            option1.grid(row=r + 2, column=1, padx=(10, 10), pady=(10, 10))
+            option2.grid(row=r + 2, column=2, padx=(10, 10), pady=(10, 10))
 
-        self.label_mode = customtkinter.CTkLabel(master=self.second_frame, text="寻车:")
-        self.label_mode.grid(row=6, column=0, columnspan=1, padx=10, pady=10, sticky="")
-
-        tabview = customtkinter.CTkTabview(self.second_frame, width=340)
-        tabview.grid(
-            row=6, column=1, columnspan=2, padx=(20, 0), pady=(20, 0), sticky="nsew"
+        carhunt_setting_frame = customtkinter.CTkFrame(self.settings, width=340)
+        carhunt_setting_frame.grid(
+            row=4, column=1, columnspan=2, padx=(20, 0), pady=(20, 0), sticky="nsew"
         )
 
-        tabview.add("寻车配置")
+        car_position = customtkinter.CTkLabel(
+            master=carhunt_setting_frame, text="车库位置:"
+        )
 
-        tabview.tab("寻车配置").grid_columnconfigure(1, weight=1)
+        car_position.grid(row=1, column=0, padx=(15, 10), pady=(10, 10))
 
-        car_position = customtkinter.CTkLabel(master=tabview.tab("寻车配置"), text="车库位置:")
-
-        car_position.grid(row=1, column=0, padx=10, pady=(10, 10))
-
-        row = customtkinter.CTkLabel(master=tabview.tab("寻车配置"), text="row")
+        row = customtkinter.CTkLabel(master=carhunt_setting_frame, text="row")
 
         row.grid(row=1, column=1, padx=(0, 0), pady=(10, 10), sticky="nsew")
 
-        col = customtkinter.CTkLabel(master=tabview.tab("寻车配置"), text="col")
+        col = customtkinter.CTkLabel(master=carhunt_setting_frame, text="col")
 
         col.grid(row=1, column=2, padx=(0, 10), pady=(10, 10), sticky="nsew")
 
         for r in range(6):
             option1 = customtkinter.CTkOptionMenu(
-                tabview.tab("寻车配置"),
+                carhunt_setting_frame,
                 dynamic_resizing=False,
                 values=[str(i) for i in range(0, 3)],
                 width=100,
@@ -306,23 +264,23 @@ class App(customtkinter.CTk):
             )
 
             option2 = customtkinter.CTkOptionMenu(
-                tabview.tab("寻车配置"),
+                carhunt_setting_frame,
                 dynamic_resizing=False,
                 values=[str(i) for i in range(0, 20)],
                 width=100,
                 height=28,
             )
 
-            option1.grid(row=r + 2, column=1, padx=(0, 0), pady=(10, 10))
-            option2.grid(row=r + 2, column=2, padx=(0, 0), pady=(10, 10))
+            option1.grid(row=r + 2, column=1, padx=(10, 10), pady=(10, 10))
+            option2.grid(row=r + 2, column=2, padx=(10, 10), pady=(10, 10))
 
         # create third frame
-        self.third_frame = customtkinter.CTkFrame(
+        self.help = customtkinter.CTkFrame(
             self, corner_radius=0, fg_color="transparent"
         )
 
         # select default frame
-        self.select_frame_by_name("frame_2")
+        self.select_frame_by_name("settings")
         self.mode_buttons.configure(values=["多人一", "多人二", "寻车"])
         self.mode_buttons.set("多人一")
 
@@ -331,11 +289,11 @@ class App(customtkinter.CTk):
         self.home_button.configure(
             fg_color=("gray75", "gray25") if name == "home" else "transparent"
         )
-        self.frame_2_button.configure(
-            fg_color=("gray75", "gray25") if name == "frame_2" else "transparent"
+        self.settings_button.configure(
+            fg_color=("gray75", "gray25") if name == "settings" else "transparent"
         )
-        self.frame_3_button.configure(
-            fg_color=("gray75", "gray25") if name == "frame_3" else "transparent"
+        self.help_button.configure(
+            fg_color=("gray75", "gray25") if name == "help" else "transparent"
         )
 
         # show selected frame
@@ -343,23 +301,23 @@ class App(customtkinter.CTk):
             self.home_frame.grid(row=0, column=1, sticky="nsew")
         else:
             self.home_frame.grid_forget()
-        if name == "frame_2":
-            self.second_frame.grid(row=0, column=1, sticky="nsew")
+        if name == "settings":
+            self.settings.grid(row=0, column=1, sticky="nsew")
         else:
-            self.second_frame.grid_forget()
-        if name == "frame_3":
-            self.third_frame.grid(row=0, column=1, sticky="nsew")
+            self.settings.grid_forget()
+        if name == "help":
+            self.help.grid(row=0, column=1, sticky="nsew")
         else:
-            self.third_frame.grid_forget()
+            self.help.grid_forget()
 
     def home_button_event(self):
         self.select_frame_by_name("home")
 
-    def frame_2_button_event(self):
-        self.select_frame_by_name("frame_2")
+    def settings_button_event(self):
+        self.select_frame_by_name("settings")
 
-    def frame_3_button_event(self):
-        self.select_frame_by_name("frame_3")
+    def help_button_event(self):
+        self.select_frame_by_name("help")
 
 
 if __name__ == "__main__":
